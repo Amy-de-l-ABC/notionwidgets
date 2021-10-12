9a8d77d93921363c04dcf61c8b524c505159d786
Amy-de-l-ABC <92403431+Amy-de-l-ABC@users.noreply.github.com>
2021-10-12 14:34:46 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+<!-- Coolors Palette Widget -->
+      <script src="https://coolors.co/palette-widget/widget.js"></script>
+      <script data-id="05092244421248284">new CoolorsPaletteWidget("05092244421248284", ["9b59b6","3498db","1abc9c","e57f24","de0d82","e74c3c","0b3954","5c574f","ffc1cf"]); </script>
